0c975d6b90fae9a816daf97829692d7b730054e1
johnwquarles <john.w.quarles@gmail.com>
2015-07-18 14:36:12 -0500

    adding files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace array_sum_evens
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("What number do you want to choose?");
+			int upperBound = int.Parse(Console.ReadLine());
+			IEnumerable<int> arr = Enumerable.Range (1, (int)Math.Floor((decimal)(upperBound / 2))).Select(i => 2 * i);
+			Console.WriteLine ("Here are the evens:");
+			foreach (int num in arr) {
+				Console.WriteLine (num);
+			}
+			Console.WriteLine ("Here is the sum of the evens " + arr.Sum());
+		}
+	}
+}
